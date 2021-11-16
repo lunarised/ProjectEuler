27f44fa877604b8cd0d0e51d47b10ac6749f40ea
lunarised <lunarised@outlook.com>
2021-11-16 13:12:52 +1300

    Written the first problem in Racket. Could probably be better!

---
+++ 1/1Racket.rkt
@@ -0,0 +1,27 @@
+#lang racket
+(define (getValue a) 
+	(if 
+		(or 
+		    ( =  (modulo a 3) 0 ) 
+		    ( = (modulo a 5) 0 )
+		    )		  
+		  a
+		  0
+	)
+)
+
+
+(define (Euler1 MAX)
+  (foldl 
+    + 
+    0 
+    (map 
+      	(lambda (a) (getValue a)) 
+	(range MAX)
+    )
+  )
+)  
+
+(Euler1 1000)
+
+
