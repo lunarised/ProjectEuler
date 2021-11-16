fc31aaf57ba56201bbb5588d157f58e3268efe9f
lunarised <lunarised@outlook.com>
2021-11-16 13:13:18 +1300

    Written 2nd Problem in racket. Loooks pre clean

---
+++ 2/2Racket.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(define (fibonacciGen a b l m)
+	(if (< a m)
+	  (fibonacciGen (+ b a) a (cons a l) m)
+	  l
+			)
+
+)
+
+(define (sumEvenFib m) 
+  ( foldl + 0 (filter (lambda (x) (= (modulo x 2) 0)) (fibonacciGen 1 1 ( list 1 ) m))))
+
+(sumEvenFib 4000000)
