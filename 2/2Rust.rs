acc6e637bd21581fa5f45d1fcbe46911951e87fc
lunarised <lunarised@outlook.com>
2021-11-19 23:40:29 +1300

    Keeping rustc happy
    ok for real... rustc can go take its compile suggestions and keep giving them to me because they are helping me write much more consistent code. Thanks rustc

--- 2/2Rust.rs
+++ 2/2Rust.rs
@@ -1,18 +1,17 @@
 fn main(){
-    let max = 1000;
-    let sum = fibGen(1, 1, 4000000, 0);
+    let sum = fib_gen(1, 1, 4000000, 0);
     println!("{}", sum);
 }
 
 
-fn fibGen(a:usize, b:usize, max:usize, sum:usize)-> usize{
-    let mut newSum = sum;
+fn fib_gen(a:usize, b:usize, max:usize, sum:usize)-> usize{
+    let mut new_sum = sum;
     let c = a + b;
     if a % 2 == 0{
-        newSum += a;
+        new_sum += a;
     }
     if a < max{
-        return fibGen(c, a, max, newSum);
+        return fib_gen(c, a, max, new_sum);
     }
     else{
         return sum;   
